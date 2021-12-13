ff758c09ed98f1165bfa943bf99f7d4359edf11f
Valeryia Azheika <valeryia_azheika@epam.com>
2021-12-13 16:50:02 +0300

    First commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+    static WebDriver driver;
+    public static void main(String[] args)  {
+
+
+    }
+
+}
